c89db09a7e994e7e25696ed9a6bad1557f251b26
Lee Penkman <leepenkman@gmail.com>
2024-08-10 19:27:23 +1200

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@ gcloud config set project evangelerapp
 
 gsutil -m rsync -r ./static/css gs://static.netwrck.com/static/css
 gsutil -m rsync -r ./static/img gs://static.netwrck.com/static/img
+gsutil -m rsync -r ./static/evimg gs://static.netwrck.com/static/evimg
 gsutil -m rsync -r ./static/js gs://static.netwrck.com/static/js
 
 gcloud app deploy --project evangelerapp
 
 # deploy index.yaml
-gcloud app deploy --project evangelerapp --no-promote index.yaml
+# gcloud app deploy --project evangelerapp --no-promote index.yaml
 
